d137634371ac23d9d6b0d6d919adc302ece5f586
Jacob Rosenthal <jakerosenthal@gmail.com>
2014-12-26 19:59:15 -0700

    bump to chrome 0.0.17 and ditch long lived connection stuff as somethings broken

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-// For long-lived connections:
-chrome.runtime.onConnectExternal.addListener(function(port) {
-  console.assert(port.name == "knockknock");
-  port.onMessageExternal.addListener(function(msg) {
-    if (msg.joke == "Knock knock")
-      port.postMessage({question: "Who's there?"});
-    else if (msg.answer == "Madame")
-      port.postMessage({question: "Madame who?"});
-    else if (msg.answer == "Madame... Bovary")
-      port.postMessage({question: "I don't get it."});
-  });
-});
-
 chrome.runtime.onMessageExternal.addListener(function(msg, sender, responder) {
 
   var cmds = {
