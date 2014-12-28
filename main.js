822d13b363ed123c0cdd8171bca19baecb6096d2
Jacob Rosenthal <jakerosenthal@gmail.com>
2014-12-27 20:12:57 -0700

    bump chrome to 0.0.26, try sending error string instead of error object

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ chrome.runtime.onMessageExternal.addListener(function(msg, sender, responder) {
         console.log(err);
         var resp = {};
         if (err){
-          resp.error = new Error(err.message);
+          resp.error = "error";
         }
         responder(resp);
       });
